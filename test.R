c81f365ee01afd2ad8904485618638d739261979
Kyra <kyra@balenzano.com>
2019-11-25 11:53:45 -0500

    Added comments before each section of code and added a single-letter sample.

--- test.R
+++ test.R
@@ -1,5 +1,10 @@
+# Printing 1 through 10:
 for (i in 1:10) {
   cat(i, "\n")
 }
 
 cat("finished", "\n")
+
+# Practice with sampling without replacement:
+letter.permutation = sample(c("a", "b", "c", "d"), size=4)
+cat(letter.permutation, "\n")
